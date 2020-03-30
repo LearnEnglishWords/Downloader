69063ba4fb292991aa1683438dc0937bd7e6f595
Martin Jablecnik <martin.jablecnik@email.cz>
2020-03-30 19:52:25 +0200

    Refactor

--- server.py
+++ server.py
@@ -2,8 +2,25 @@
 
 from flask import Flask
 from flask import render_template, request
-import urllib, json
+import urllib, json, hashlib
 from bs4 import BeautifulSoup
+from time import sleep
+
+
+def text_to_speech(text, voice):
+    data = bytes(json.dumps({"engine": "Google", "data": {"text": text, "voice": voice}}), encoding='utf-8')
+    req =  urllib.request.Request("https://api.soundoftext.com/sounds", data=data)
+    req.add_header('Content-Type', 'application/json')
+    resp = urllib.request.urlopen(req)
+
+    code_name = json.loads(resp.read())["id"]
+    url = "https://soundoftext.nyc3.digitaloceanspaces.com/{0}.mp3".format(code_name)
+    return url
+
+
+def get_hash(text, voice, speed='normal'):
+    return hashlib.md5((text + voice + speed).encode()).hexdigest()
+
 
 
 app = Flask(__name__)
@@ -24,45 +41,27 @@ def download_word():
     if voice != "en-GB" and voice != "en-US":
         return {"status": 400, "message": "Voice is in wrong format."}
 
-    data = bytes(json.dumps({"engine": "Google", "data": {"text": text, "voice": voice}}), encoding='utf-8')
-    req =  urllib.request.Request("https://api.soundoftext.com/sounds", data=data)
-    req.add_header('Content-Type', 'application/json')
-
-    resp = urllib.request.urlopen(req)
-
-    codeName = json.loads(resp.read())["id"]
-    url = "https://soundoftext.nyc3.digitaloceanspaces.com/{0}.mp3".format(codeName)
+    url = text_to_speech(text, voice)
+    sleep(1)
     urllib.request.urlretrieve(url, "static/sounds/words/{0}-{1}.mp3".format(text, voice.split("-")[1].lower()))
 
     return {"status": 200}
 
 
-@app.route('/download/sentence', methods=['GET'])
+@app.route('/download/sentence', methods=['POST'])
 def download_sentence():
-    text = request.args.get('text', '')
-    voice = request.args.get('voice', 'en-GB')
+    text = request.form['text']
+    voice = request.form['voice']
 
     if text == '':
         return {"status": 400, "message": "Sentence text is missing."}
     if voice != "en-GB" and voice != "en-US":
         return {"status": 400, "message": "Voice is in wrong format."}
 
-    language = "British+English"
-    voice = "IVONA+Amy22+%28UK+English%29"
-
-    data = bytes("input_text={0}&language={1}&voice={2}&speed=-1&action=process_text".format(text, language, voice), encoding='utf-8')
-    req =  urllib.request.Request("http://www.fromtexttospeech.com", data=data)
-    req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-
-    resp = urllib.request.urlopen(req)
-
-    print(resp.read())
-    soup = BeautifulSoup(resp.read(), 'html.parser')
-    for link in soup.find_all('a'):
-        print(link.get('href'))
-
-    #codeName = json.loads(resp.read())["id"]
-    #url = "https://soundoftext.nyc3.digitaloceanspaces.com/{0}.mp3".format(codeName)
-    #urllib.request.urlretrieve(url, "static/sounds/words/{0}-{1}.mp3".format(text, voice.split("-")[1].lower()))
+    url = text_to_speech(text, voice)
+    sleep(1)
+    urllib.request.urlretrieve(url, "static/sounds/examples/{0}.mp3".format(get_hash(text, voice)))
 
     return {"status": 200}
+
+
